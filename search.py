bfe7428e7669f591453a8aa4dd676c1d7ba64c2c
sunhao <sunhao@indemind.cn>
2020-10-15 18:46:09 +0800

    add search

--- search.py
+++ search.py
@@ -6,6 +6,7 @@ import argparse
 import  sys, os
 from  utils import WriteTxt
 from arxiv import *
+import re
 
 CHROME_DRIVER = "/media/hao/DS/OTHER/TOOLS"
 sys.path.append("CHROME_DRIVER")
@@ -15,6 +16,7 @@ ONLY_NEW = True
 
 NUM_FILE = "paper_number.csv"
 ITEMS_NUM = 200
+SERCH_TYPE = 'title' # all
 
 def GetArgs():
     parse = argparse.ArgumentParser()
@@ -24,21 +26,38 @@ def GetArgs():
     return parse.parse_args()
 
 def GetPages(baseurl, content):
-    content_root = "/html/body/main/div[1]/div/h1/text()"[0]
+    content_root = "/html/body/main/div[1]/div/h1/text()"
+    num = content.xpath(content_root)[0]
+    num = re.findall(r"\d+\.?\d*", num)
+    num = [int(n) for n in num]
+    total_num = int((num[2]+num[1]-1) / num[1])
 
-    total_num = 0
-    items = range(0, total_num, 200)
-    urls = ["{}&start={}".format(baseurl, ITEMS_NUM*i) + i for i in items]
+    urls = [baseurl, ]
+    if total_num > 1:
+        items = range(0, total_num, num[1])
+        urls = ["{}&start={}".format(baseurl, ITEMS_NUM*i) + i for i in items]
 
     return urls
 
+def GetArticalUrl(driver, page_urls):
+    urls = []
+    for url in page_urls:
+        driver.get(url)
+        page_content = GetContent(driver)
+        artical = ''
+        urls.append(artical)
+
+    return  urls
+
+
 def ParseArXiv(driver, key, output_dir:str):
-    baseurl = "https://arxiv.org/search/cs?query={}&searchtype=all&abstracts=show&order=-announced_date_first&size={}".format(key, ITEMS_NUM)
+    baseurl = "https://arxiv.org/search/cs?query={}&searchtype={}&abstracts=show&order=-announced_date_first&size={}".format(key, SERCH_TYPE, ITEMS_NUM)
     driver.get(baseurl)
     time.sleep(1)
 
     page_content = GetContent(driver)
-    urls = GetPages(page_content)
+    page_urls = GetPages(baseurl, page_content)
+    urls = GetArticalUrl(driver, page_urls)
     file = os.path.join(output_dir, "{}.md".format(key))
 
     for page in tqdm(urls):
@@ -53,18 +72,10 @@ def main():
     args = GetArgs()
     driver = webdriver.Chrome()
 
-    try:
-        items_num = []
-        num, date = ParseArXiv(driver, args.query, args.output_dir)
-        items_num.append(num)
-        print(date)
-        file = os.path.join(args.output_dir, NUM_FILE)
-        items_num = [str(i) for i in items_num]
-        txt = "{},".format(date) + ",".join(items_num)
-        WriteTxt(txt, file, "a")
-        driver.close()
-    except:
-        driver.close()
+    items_num = []
+    num = ParseArXiv(driver, args.query, args.output_dir)
+    print("num: ", num)
+    driver.close()
 
 
 if __name__ == "__main__":
