d4530bb260f3457352ed2c367288844bf47e9194
smartadpole <smartadpole@gmail.com>
2024-08-26 15:20:46 +0800

    remove unused code

--- text/architectural_119.py
+++ text/architectural_119.py
@@ -13,11 +13,9 @@ CURRENT_DIR = os.path.dirname(__file__)
 sys.path.append(os.path.join(CURRENT_DIR, '../'))
 
 import math
-import requests
 import time
 from tqdm import tqdm
 import argparse
-import sys
 import os
 import urllib
 import re
@@ -25,7 +23,7 @@ from util.file import WriteTxt, MkdirSimple
 import csv
 import io
 import markdown2
-from weasyprint import HTML, CSS
+from weasyprint import HTML
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
@@ -43,133 +41,9 @@ def GetArgs():
     parse = argparse.ArgumentParser()
     parse.add_argument("--url", type=str)
     parse.add_argument("--output_dir", type=str, default="./file")
+    parse.add_argument("--type", type=str, choices=['pdf', 'markdown'], default="pdf")
 
     return parse.parse_args()
-
-def RemoveTags(text):
-    # 匹配尖括号内的内容
-    pattern = r'<.*?>'
-    # 使用 sub 函数替换匹配到的内容为空字符串
-    return re.sub(pattern, '', text)
-
-def GetPDFUrl(content, min_id):
-    global INVALID
-    items = []
-
-    if INVALID:
-        return items
-
-    li_matches = re.finditer(r'<li class="arxiv-result">.*?</li>', content, re.DOTALL)
-    for li_match in li_matches:
-        li_content = li_match.group()
-        label = re.findall(r'">([^<]+)</span>', li_content)
-        pdf_match = re.search(r'<a href="([^"]+)">pdf</a>', li_content)
-        title_match = re.search(r'<p class="title is-5 mathjax">\s*(.*?)\s*</p>', li_content, re.DOTALL)
-        if len(label) > 0 and pdf_match and title_match:
-            label = "cs.CV" if "cs.CV" in label else label[0]
-            pdf_url = pdf_match.group(1)
-            title = title_match.group(1)
-            title = RemoveTags(title)
-
-            if "" != min_id:
-                pdf_id = pdf_url.split('/')[-1]
-                if pdf_id == min_id:
-                    INVALID = True
-                    return items
-
-            items.append({"url": pdf_url, "label": label, "title": title})
-
-    return items
-
-
-def GetPages(baseurl, content):
-    urls = [baseurl, ]
-
-    total_num = re.search(r'of (\d{1,3}(,\d{3})*(\.\d+)?)', content)
-
-    if total_num:
-        total_num = int(total_num.group(1).replace(',', ''))
-        total_pages = math.ceil(total_num / ITEMS_NUM)
-
-        if total_pages > 1:
-            items = list(range(0, total_pages))
-            urls = ["{}&start={}".format(baseurl, str(ITEMS_NUM * i)) for i in items]
-
-    return urls
-
-def GetArticalUrl(page_urls, min_id):
-    global INVALID
-    urls = []
-
-    base_urls = page_urls if isinstance(page_urls, list) else [page_urls, ]
-
-    for url in base_urls:
-        if INVALID:
-            return urls
-        response = requests.get(url)
-        if response.status_code == 200:
-            content = response.text
-            print("loading papers info from {}".format(url))
-            pdf_links = GetPDFUrl(content, min_id)
-            urls.extend(pdf_links)
-        time.sleep(1)
-
-    return urls
-
-
-def ParseArXiv(key, min_id):
-    if key.split()[0].lower() in COMMENTS:
-        baseurl = "https://arxiv.org/search/?query={}&searchtype={}&abstracts=show&order=-announced_date_first&size={}".format(key, "comments", ITEMS_NUM)
-    else:
-        if '+' in key:
-            baseurl = "https://arxiv.org/search/?query={}&searchtype={}&abstracts=show&order=-announced_date_first&size={}".format(key, SERCH_TYPE, ITEMS_NUM)
-        else:
-            baseurl = "https://arxiv.org/search/?query=\"{}\"&searchtype={}&abstracts=show&order=-announced_date_first&size={}".format(key, SERCH_TYPE, ITEMS_NUM)
-    response = requests.get(baseurl)
-    if response.status_code == 200:
-        content = response.text
-        page_urls = GetPages(baseurl, content)
-        urls = GetArticalUrl(page_urls, min_id)
-
-        return urls
-    else:
-        print("Response URL:", response.url)
-        print("Response Status Code:", response.status_code)
-        print("Response Reason:", response.reason)
-        print("Response Elapsed Time:", response.elapsed)
-
-    return []
-
-
-def Download(items, output: str, usetitle=False):
-    os.makedirs(output, exist_ok=True)
-
-    for item in tqdm(items):
-        try:
-            url = item['url']
-            if usetitle:
-                name = item['title']
-            else:
-                name = os.path.basename(url)
-            label = item['label']
-            file_path = os.path.join(output, label, name + ".pdf")
-            MkdirSimple(file_path)
-            urllib.request.urlretrieve(url, file_path)
-        except:
-            print("error download {}.".format(item))
-
-def SaveCSV(items, file):
-    MkdirSimple(file)
-    output = io.StringIO()
-    writer = csv.writer(output, quoting=csv.QUOTE_MINIMAL)
-
-    for item in items:
-        cleaned_values = [value.strip().replace('\n', ' ').replace('\r', ' ') for value in item.values()]
-        writer.writerow(cleaned_values)
-
-    text = output.getvalue().strip()
-    WriteTxt(text, file, 'w')
-
 
 def get_page_content(url):
     """Get the content of the page."""
@@ -356,10 +230,6 @@ def parse_page(url, output_dir, output_dir_image):
 
     title, markdown_content, html_content = convert_html_to_markdown(main_content, url, output_dir, output_dir_image)
     title = title.replace('/', '_').replace(' ', '_')
-
-    global flag
-    if '600问' in title:
-        flag = True
 
     has_toc = soup.find_all('a', class_='round_button')
     if has_toc:
